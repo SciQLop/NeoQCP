149405deb391f0b1591e81d6d3bee773f68daa6b
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2025-03-02 23:04:53 +0100

    Fix high dpi issue on macos

--- src/core.cpp
+++ src/core.cpp
@@ -2243,7 +2243,11 @@ void QCustomPlot::paintEvent(QPaintEvent *event)
   // detect if the device pixel ratio has changed (e.g. moving window between different DPI screens), and adapt buffers if necessary:
 #ifdef QCP_DEVICEPIXELRATIO_SUPPORTED
 #  ifdef QCP_DEVICEPIXELRATIO_FLOAT
+#ifdef Q_OS_MAC
+  double newDpr = 1.; // not sure why
+#else
   double newDpr = devicePixelRatioF();
+#endif
 #  else
   double newDpr = devicePixelRatio();
 #  endif
